027175024443af650a2c2fab6905a1cc68b9174f
shanenoi <shanenoi.org@gmail.com>
2021-03-07 15:28:01 +0700

    finish

---
+++ src/test/test_vars.c
@@ -0,0 +1,28 @@
+#include "../vars.h"
+#include <assert.h>
+
+void test_seperater() {
+    #ifdef _WIN32 || _WIN64
+        assert(SEPERATER == "\\");
+    #elif __linux__
+        assert(SEPERATER == "/");
+    #endif
+}
+
+void test_pwd() {
+    char* temp = pwd();
+    assert(temp != "");
+    free(temp);
+}
+
+void test_join_path() {
+    char* temp = join_path((char*)"a", (char*)"b");
+    assert(temp != "");
+    free(temp);
+}
+
+int main() {
+    test_seperater();
+    test_pwd();
+    test_join_path();
+}
--- src/vars.h
+++ src/vars.h
@@ -1,6 +1,9 @@
 #ifndef VARS
 #define VARS
 
+#include <stdlib.h>
+#include <string.h>
+
 #define FILENAME_MAX 125
 
 #ifdef _WIN32 || _WIN64
@@ -8,19 +11,30 @@
     #define SEPERATER "\\"
     // get current work direction
     #include <direct.h>
-    #define pwd _getcwd
+    #define _pwd _getcwd
 
 #elif __linux__
     // set seperater
     #define SEPERATER "/"
     // get current work direction
     #include <unistd.h>
-    #define pwd getcwd
+    #define _pwd getcwd
 #endif
 
 
 char* pwd(void) {
-    char PATH[FILENAME_MAX];
+    char* path = (char*) malloc(FILENAME_MAX);
+    _pwd(path, FILENAME_MAX);
+    return path;
+}
+
+char* join_path(char* path1, char* path2) {
+    char* result = (char*) malloc(strlen(path1) + strlen(path2) + 2);
+    strcpy(result, path1);
+    strcat(result, SEPERATER);
+    strcat(result, path2);
+    return result;
 }
 
 #endif /*VARS*/
+
