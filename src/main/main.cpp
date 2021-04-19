16b94745f909b4089cd69d090181a6ca825ed80f
Bao Dang <danglequocbao001@gmail.com>
2021-04-19 20:26:12 +0700

    feat: main menu

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,196 +1,101 @@
-#include <stdio.h>
-#include <string.h>
-
-void hienThiTenCot();
-
-struct HoTen {
-	char ho[20];
-	char dem[20];
-	char ten[20];
-};
-
-struct DiemMH {
-	float toan;
-	float van;
-	float anh;
-	float tbc;
-};
-
-struct SinhVien {
-	int ma;
-	struct HoTen hoVaTen;
-	int tuoi;
-	char gioiTinh[10];
-	struct DiemMH diem;
-};
-
-void nhapDiem(struct DiemMH* diem) {
-	printf("Toan: ");
-	scanf("%f", &diem->toan);
-	printf("Van: ");
-	scanf("%f", &diem->van);
-	printf("Anh: ");
-	scanf("%f", &diem->anh);
-	diem->tbc = (diem->toan + diem->van + diem->anh) / 3;
-}
-
-void nhapHoTen(struct HoTen* ten) {
-	printf("Ho: ");
-	scanf("%s", ten->ho);
-	printf("Dem: ");
-	getchar();
-	gets(ten->dem);
-	printf("Ten: ");
-	scanf("%s", ten->ten);
-}
-
-struct SinhVien nhapSV() {
-	struct SinhVien sv;
-	printf("Nhap ma: ");
-	scanf("%d", &sv.ma);
-	nhapHoTen(&sv.hoVaTen);
-	printf("Tuoi: ");
-	scanf("%d", &sv.tuoi);
-	printf("Gioi tinh: ");
-	scanf("%s", sv.gioiTinh);
-	nhapDiem(&sv.diem);
-	return sv;
-}
-
-void hienThiTTSV(struct SinhVien sv) {
-	printf("%-10d %-10s %-20s %-10s %-10d %-10s %-10.2f %-10.2f %-10.2f %-10.2f\n",
-	sv.ma, sv.hoVaTen.ho, sv.hoVaTen.dem, sv.hoVaTen.ten, sv.tuoi, sv.gioiTinh,
-	sv.diem.toan, sv.diem.van, sv.diem.anh, sv.diem.tbc);
-}
-
-void sapXepTheoTen(struct SinhVien* ds, int slsv) {
-	int i, j;
-	for(i = 0; i < slsv - 1; i++) {
-		for(j = slsv - 1; j > i; j --) {
-			if(strcmp(ds[j].hoVaTen.ten, ds[j-1].hoVaTen.ten) < 0) {
-				struct SinhVien sv = ds[j];
-				ds[j] = ds[j - 1];
-				ds[j - 1] = sv;
-			}
-		}
-	}
-}
-
-void sapXepTheoDiem(struct SinhVien* ds, int slsv) {
-	int i, j;
-	for(i = 0; i < slsv - 1; i++) {
-		for(j = slsv - 1; j > i; j --) {
-			if(ds[j].diem.tbc > ds[j - 1].diem.tbc) {
-				struct SinhVien sv = ds[j];
-				ds[j] = ds[j - 1];
-				ds[j - 1] = sv;
-			}
-		}
-	}
-}
-
-void timTheoTen(struct SinhVien* ds, int slsv) {
-	char ten[20];
-	printf("Nhap ten: ");
-	scanf("%s", ten);
-	
-	hienThiTenCot();
-	
-	int i, timSV = 0;
-	for(i = 0; i < slsv; i++) {
-		if(strcmp(ten, ds[i].hoVaTen.ten) == 0) {
-			hienThiTTSV(ds[i]);
-			timSV = 1;
-		}
-	}
-	if(timSV == 0) {
-		printf("Khong co sinh vien %s trong danh sach!\n", ten);
-	}
-}
-
-void ghiFile(struct SinhVien* ds, int slsv) {
-	FILE* fout = fopen("SV.txt", "a");
-	int i;
-	for(i = 0; i < slsv; i++) {
-		struct SinhVien sv = ds[i];
-		fprintf(fout, "%-10d %-10s %-20s %-10s %-10d %-10s %-10.2f %-10.2f %-10.2f %-10.2f\n",
-		sv.ma, sv.hoVaTen.ho, sv.hoVaTen.dem, sv.hoVaTen.ten, sv.tuoi, sv.gioiTinh,
-		sv.diem.toan, sv.diem.van, sv.diem.anh, sv.diem.tbc);
-	}
-	fclose(fout);
-}
-
-void hienThiTenCot() {
-	printf("%-10s %-10s %-20s %-10s %-10s %-10s %-10s %-10s %-10s %-10s\n", 
-		"Ma SV", "Ho", "Dem", "Ten", "Tuoi", "Gioi Tinh", 
-		"Diem Toan", "Diem Van", "Diem Anh", "Diem TBC");
-}
+#include<windows.h>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
 int main() {
-	struct SinhVien dssv[100];
-	int slsv = 0;
-	int luaChon;
-	
-	do {
-		printf("=============== MENU ===============");
-		printf("\n1. Them Sinh vien vao danh sach.");
-		printf("\n2. Hien thi danh sach sinh vien.");
-		printf("\n3. Sap xep theo ten.");
-		printf("\n4. Sap xep theo diem giam dan.");
-		printf("\n5. Tim sinh vien theo ten.");
-		printf("\n6. Ghi thong tin sinh vien ra file.");
-		printf("\n0. Thoat chuong trinh.");
-		printf("\nBan chon ? ");
-		
-		scanf("%d", &luaChon);
-		struct SinhVien sv;
-		int i;
-		switch(luaChon) {
-			case 0:
-				break;
-				
-			case 1:
-				sv = nhapSV();
-				dssv[slsv++] = sv;
-				break;
-				
-			case 2:
-				hienThiTenCot();
-				for(i = 0; i < slsv; i++) {
-					hienThiTTSV(dssv[i]);
-				}
-				break;
-				
-			case 3:
-				sapXepTheoTen(dssv, slsv);
-				hienThiTenCot();
-				for(i = 0; i < slsv; i++) {
-					hienThiTTSV(dssv[i]);
-				}
-				break;
-				
-			case 4:
-				sapXepTheoDiem(dssv, slsv);
-				hienThiTenCot();
-				for(i = 0; i < slsv; i++) {
-					hienThiTTSV(dssv[i]);
-				}
-				break;
-				
-			case 5:
-				timTheoTen(dssv, slsv);
-				break;
-				
-			case 6:
-				ghiFile(dssv, slsv);
-				break;
-				
-			default:
-				printf("Sai chuc nang, vui long chon lai!\n");
-				break;
-		}
-		
-	} while(luaChon);
-	
-	return 0;
+	::SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000); //full window screen
+    system("cls");
+    string Menu[4] = { "The doc gia", "Thong tin cac danh sach", "Tra sach", "Thoat" };
+    int pointer = 0;
+    bool bMainMenu = true;
+
+    while (bMainMenu)
+    {
+        system("cls");
+
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+        cout << "Main Menu\n\n";
+
+        for (int i = 0; i < 4; ++i)
+        {
+            if (i == pointer)
+            {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+                cout << Menu[i] << endl;
+            }
+            else
+            {
+                SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+                cout << Menu[i] << endl;
+            }
+        }
+
+        while (bMainMenu)
+        {
+            if (GetAsyncKeyState(VK_UP)&1)
+            {
+                pointer = pointer - 1;
+                if (pointer == -1)
+                {
+                    pointer = 2;
+                }
+                break;
+            }
+            else if (GetAsyncKeyState(VK_DOWN)&1)
+            {
+                pointer += 1;
+                if (pointer == 4)
+                {
+                    pointer = 0;
+                }
+                break;
+            }
+            else if (GetAsyncKeyState(VK_RETURN)&1)
+            {
+                switch (pointer)
+                {
+                case 0:
+                {
+                    //admin_login();
+                    system("cls");
+                    cout << "admin selected\n";
+                    bMainMenu = false;
+                    break;
+                }
+                case 1:
+                {
+                    //customer_sign();
+                    system("cls");
+                    cout << "customer selected\n";
+                    bMainMenu = false;
+                    break;
+                }
+                case 2:
+                {
+                    //thank_you();
+                    system("cls");
+                    cout << "thank you selected\n";
+                    bMainMenu = false;
+                    break;
+                }
+                case 3:
+                {
+                    //thank_you();
+                    system("cls");
+                    cout << "exit selected\n";
+                    std::terminate();
+                    bMainMenu = false;
+                    break;
+                }
+                default:
+                {
+                    cout << "Invalid Input! ";
+                    std::terminate();
+                }
+                }
+            }
+        }
+    }
+    return 0;
 }
