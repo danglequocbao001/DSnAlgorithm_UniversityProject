2982e30f6627688885693bcaaab36df3168fbd39
BotOfShane <shanenoi.org@gmail.com>
2021-05-31 21:24:53 +0700

    fix-somethings

--- src/models/models.h
+++ src/models/models.h
@@ -1,40 +1,56 @@
 #include "__init__.h"
 
 /*
- * Model<DanhMucSachSchema, LinkedList>* DanhMucSach = new Model<>();
+ * TODO:
+ *      Model<DanhMucSachSchema, LinkedList>* DanhMucSach = new Model<>();
+ *       - actions:
+ *           + load from db
+ *           + write to db
  */
 
-template <typename T, class C>
+template <class T, template <class T> class C>
 class Model {
-    private:
-        struct string_2d split_string(char*, char);
+    // private:
+    public:
+        int count_substring(char*, char*);
+        struct string_2d split_string(char*, char*);
 
     public:
         C<T>* data;
-        T str2attr(char* string)
+        T str2attr(char* string);
         Model();
         ~Model();
 };
 
 
 /*____PRIVATE BLOCK____*/
-template <typename T, class C>
+template <class T, template <class T> class C>
+int Model<T, C>::count_substring(char* string, char* chr) {
+    int len_string = strlen(string),
+        len_chr = strlen(chr),
+        counted_chrs = 0;
+
+    for (int index = 0; index < len_string; index++)
+        for(int _index = index; _index < index + len_chr; _index++)
+            if (string[_index] == chr[_index - index])
+                counted_chrs++;
+
+    return counted_chrs;
+}
+
+
+template <class T, template <class T> class C>
 struct string_2d Model<T, C>::split_string(char* string, char* chr) {
 
-    int len_string = strlen(string);
-    int len_chr = strlen(chr);
-    int counted_chrs = 0;
+    int len_string = strlen(string),
+        len_chr = strlen(chr),
+        counted_chrs = 0;
 
     char _string[len_string];
     strcpy(_string, string);
 
-    /*__COUNT AMOUNT OF CHR IN STRING__*/
-    for (int index = 0; index < len_string; index ++)
-        for(int _index = index; _index < index + len_chr; _index ++ )
-            if (string[_index] == chr[_index - index])
-                counted_chrs++;
-
-    char **str_array = (char**) malloc(++counted_chrs);
+    counted_chrs = Model<T, C>::count_substring(string, char);
+    char **str_array = (char**)malloc(++counted_chrs);
 
     /*__SPLIT STRING WITH CHR__*/
     char * token = strtok(_string, chr);
@@ -53,13 +69,13 @@ struct string_2d Model<T, C>::split_string(char* string, char* chr) {
 
 
 /*____PUBLIC BLOCK____*/
-template <typename T, class C>
-Model::Model() {
+template <class T, template <class T> class C>
+Model<T, C>::Model() {
     Model::data = new C<T>();
 }
 
 
-template <typename T, class C>
-Model::~Model() {
+template <class T, template <class T> class C>
+Model<T, C>::~Model() {
     Model::data = new C<T>();
 }
--- src/test/test_model.c
+++ src/test/test_model.c
@@ -1,35 +1,6 @@
 #include "../models/models.h"
 
-Singleton* Singleton::singleton_= nullptr;;
-
-Singleton *Singleton::GetInstance(const std::string& value) {
-    if(singleton_==nullptr){
-        singleton_ = new Singleton(value);
-    }
-    return singleton_;
-}
-
-void ThreadFoo(){
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    Singleton* singleton = Singleton::GetInstance("FOO");
-    std::cout << singleton->value() << "\n";
-}
-
-void ThreadBar(){
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    Singleton* singleton = Singleton::GetInstance("BAR");
-    std::cout << singleton->value() << "\n";
-}
 
 int main() {
-    std::cout <<"If you see the same value, then singleton was reused (yay!\n" <<
-        "If you see different values, then 2 singletons were created (booo!!)\n\n" <<
-        "RESULT:\n";   
-    std::thread t1(ThreadFoo);
-    std::thread t2(ThreadBar);
-    t1.join();
-    t2.join();
-
-    return 0;
 }
 
