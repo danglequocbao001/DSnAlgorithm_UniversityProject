8a70b4b1e52696868a97f776eeead819c9efb1b6
Dang Le Quoc Bao <danglequocbao001@gmail.com>
2021-07-08 23:43:12 +0700

    fix: thongbao (#42)

--- src/model/model/dau_sach.h
+++ src/model/model/dau_sach.h
@@ -717,7 +717,7 @@ void Xuat_thong_tin_cac_dau_sach(DS_DAU_SACH ds_dau_sach)
                     cout << "DANH MUC SACH";
                     khung_xuat_dms(27, 13, 20);
                     Xuat_dms_cua_1_dau_sach(ds_dau_sach.list[chon]->ds_danh_muc_sach_cua_dau_sach, 16);
-                    thong_bao("                                   ");
+                    thong_bao(" ");
                     break;
                 case 77:
                     if (i == ds_dau_sach.so_luong - 1)
